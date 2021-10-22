d6f344f7393d7f0d8aaa382732540c13c4dce874
Dávid Bangó <ynvaser@hotmail.com>
2021-10-22 20:10:30 +0200

    Spring Boot Skeleton

---
+++ src/main/java/systems/bdev/vipicsv/VipicsvApplication.java
@@ -0,0 +1,13 @@
+package systems.bdev.vipicsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipicsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipicsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/systems/bdev/vipicsv/VipicsvApplicationTests.java
@@ -0,0 +1,13 @@
+package systems.bdev.vipicsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipicsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
